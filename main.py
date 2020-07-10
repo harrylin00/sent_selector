80296fbf159f47b83fadb5b2032d34fa2bf50d01
Ubuntu <ubuntu@ip-172-31-62-108.ec2.internal>
2020-07-10 13:26:16 +0000

    first commit

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def set_config():
         'dropout': 0.2,
 
         'is_load_model': True,
-        'batch_size': 64,
-        'train_epochs': 5,
+        'batch_size': 128,
+        'train_epochs': 10,
         'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
         'accumulate_step': 1
     }
@@ -38,8 +38,8 @@ def main():
     config = set_config()
 
     # data loading
-    # train_dict_list = tp.read_jsonl_to_list_dict(config['train_data_path'])
-    # train_dataloader = data.DataLoader(train_dict_list, batch_size=config['batch_size'], shuffle=True, collate_fn=lambda x:x)
+    train_dict_list = tp.read_jsonl_to_list_dict(config['train_data_path'])
+    train_dataloader = data.DataLoader(train_dict_list, batch_size=config['batch_size'], shuffle=True, collate_fn=lambda x:x)
 
     dev_dict_list = tp.read_jsonl_to_list_dict(config['dev_data_path'])
     dev_dataloader = data.DataLoader(dev_dict_list, batch_size=config['batch_size'], shuffle=False,
@@ -57,7 +57,7 @@ def main():
     optimizer = optim.Adam(model.parameters())
 
     # training and predict
-    train.train(config, dev_dataloader, dev_dataloader, model, optimizer, word2idx)
+    train.train(config, train_dataloader, dev_dataloader, model, optimizer, word2idx)
 
     # model save
     torch.save(model.state_dict(), config['model_write_path'])
