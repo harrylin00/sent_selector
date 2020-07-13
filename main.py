add precision eval and bug fix

main.py
@@ -18,6 +18,7 @@ def set_config(embed_method='glove'):
 
         'embed_method': embed_method,
         'bert_config': 'bert-base-uncased',
+        'freeze_bert': False,
 
         'relevant_num': 1,
         'irrelevant_num': 5,
@@ -26,7 +27,7 @@ def set_config(embed_method='glove'):
         'num_layer': 3,
         'dropout': 0.2,
 
-        'is_load_model': False,
+        'is_load_model': True,
         'is_train': True,
         'batch_size': 128,
         'train_epochs': 10,
@@ -63,6 +64,7 @@ def bert_train(config, train_dataloader, dev_dataloader):
 
     model = SentSelector(config, word_embed=word_embed, bert_config=bert_config)
     if os.path.exists(config['model_load_path']) and config['is_load_model']:
+        print('begin to load model:', config['model_load_path'])
         model.load_state_dict(torch.load(config['model_load_path']))
 
     # optimizer setting

sent_selector.py
@@ -17,8 +17,8 @@ class SentSelector(nn.Module):
             self.word_embed = nn.Embedding.from_pretrained(torch.FloatTensor(word_embed), freeze=False, padding_idx=0)
         elif config['embed_method'] == 'bert':
             self.word_embed = word_embed
-            self.word_embed.weight.requires_grad=False   # for Bert model, we will freeze the word embeddings
-        # self.word_embed = nn.Embedding.from_pretrained(torch.FloatTensor(word_embed), freeze=False, padding_idx=0)
+            if config['freeze_bert']:
+                self.word_embed.weight.requires_grad=False   # for Bert model, we will freeze the word embeddings
 
         self.dropout = nn.Dropout(self.dropout_prob)
 

trivia_preprocess.py
@@ -175,7 +175,7 @@ def eval_precision_topk(similarity, labels, k):
         if len(label) <= k or sum(label) == 0:
             continue
         sort_idx = torch.argsort(sim, descending=True)
-        topk_list = [label[idx] for i, idx in enumerate(sort_idx[:k])]
+        topk_list = [label[idx] for i, idx in enumerate(sort_idx[:min(k, sum(label))])]
         precision.append(np.mean(topk_list))
     return np.mean(precision)
 